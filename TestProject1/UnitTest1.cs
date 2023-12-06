1e00456e3cc32ffdd972fc36e6fbe6e9b64d3591
Aleksandr Kalinin <akalinin@sfedu.ru>
2023-12-06 12:34:00 +0300

    kek

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,6 +1,7 @@
 
+using Microsoft.VisualStudio.TestPlatform.TestHost;
 using Assert = NUnit.Framework.Assert;
-
+using  Kek2;
 namespace TestProject1
 {
     public class Tests
@@ -13,7 +14,18 @@ namespace TestProject1
         [Test]
         public void Test1()
         {
-            Assert.AreEqual(0, 0);
+            double a = 1;
+            double b = -4;
+            double c = 4;
+            var p = new Class();
+            
+            List<double> roots = Class.SolveQuadraticEquation(a, b, c);
+            List<double> expected_roots = new List<double> { 2 };
+            Assert.AreEqual(0, 0); // to check that we are ready to test
+
+            Assert.AreEqual(roots.Count, expected_roots.Count);
+
+            Assert.AreEqual(roots.First(), expected_roots.First());
         }
     }
 }
