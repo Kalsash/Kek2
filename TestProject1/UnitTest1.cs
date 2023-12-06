7a297d73b502e1784eea421d10e11139a71cc6db
Aleksandr Kalinin <akalinin@sfedu.ru>
2023-12-06 11:52:04 +0300

    Добавьте файлы проекта.

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+
+using Assert = NUnit.Framework.Assert;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(0, 0);
+        }
+    }
+}
